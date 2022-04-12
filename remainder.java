55c8d9a2a6df1f94d79a56057aa81aac97d733e0
Divi <venkata-sai-naveen.divi@capgemini.com>
2022-04-12 10:51:10 +0530

    even branch commit

--- remainder.java
+++ remainder.java
@@ -3,7 +3,14 @@ class remainder
 public static void main(String[] args)
 {
 int x = 100;
-int z = 100 % 2;
-System.out.println(z);
+if(x % 2 == 0)
+{
+
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
 }
 }
